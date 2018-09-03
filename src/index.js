e86874c8d3f26022cf8a464b7c28d835562d01cd
Alek Merani <alekmerani@gmail.com>
2018-09-03 15:22:24 -0500

    Add site versioning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import pkg from "./package.alias.json";
 
 const App = () => (
   <div className="App">
     <p>Under Construction! Please check back later...</p>
+    <p>version {pkg.version}</p>
   </div>
 );
 
